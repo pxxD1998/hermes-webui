9703833349662eb2b2de7baa96e9ec7d9509b082
pxxD1998 <pxxD1998@gmail.com>
2026-08-22 18:49:02 +0800

    test: cover entity-balanced quotes in real SMD modes

--- tests/test_smd_media_in_stream.py
+++ tests/test_smd_media_in_stream.py
@@ -1,3 +1,9 @@
     def test_real_smd_parser_detaches_balanced_quotes_in_safe_fade_split_and_tail_paths(self):
-        for case_name in ("quotedDouble", "quotedSingleSplit"):
+        for case_name in (
+            "quotedDouble",
+            "quotedSingleSplit",
+            "entityQuotedDoubleSplit",
+            "entityQuotedSingleEnd",
+            "entityQuotedDoubleOpenerSplit",
+        ):
             for mode, result in self.cases["boundaries"][case_name].items():
