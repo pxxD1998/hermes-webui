82b34dbe7d4dddd16ba64aa11365614a4e430407
pxxD1998 <pxxD1998@gmail.com>
2026-08-22 18:03:48 +0800

    fix(renderer): align MEDIA entity-quote consumers

--- tests/test_smd_media_in_stream.py
+++ tests/test_smd_media_in_stream.py
@@ -1,11 +1,20 @@
         "  wrappedRemoteQueryPunctuation:renderModes(['**MEDIA:https://example.com/a.png?signature=value.**. ']),\n"
         "  quotedDouble:renderModes(['\\\"MEDIA:/tmp/report.xlsx\\\". ']),\n"
         "  quotedSingleSplit:renderModes([\\\"'MEDIA:/tmp/report.\\\", \\\"xlsx'.\\\"]),\n"
+        "  entityQuotedDoubleSplit:renderModes(['&quot;', 'MEDIA:/tmp/report.xlsx&quot;. ']),\n"
+        "  entityQuotedSingleEnd:renderModes(['&#39;MEDIA:/tmp/report.xlsx&#39;.']),\n"
+        "  entityQuotedDoubleOpenerSplit:renderModes(['&quo', 't;MEDIA:/tmp/report.xlsx&quot;. ']),\n"
         "  quotedRemoteQuery:renderModes(['\\\"MEDIA:https://example.com/a.png?signature=value!\\\". ']),\n"
         "  quotedRemoteFragment:renderModes(['\\\"MEDIA:https://example.com/a.png#preview!\\\". ']),\n"
 
     def test_real_smd_parser_detaches_balanced_quotes_in_safe_fade_split_and_tail_paths(self):
-        for case_name in ("quotedDouble", "quotedSingleSplit"):
+        for case_name in (
+            "quotedDouble",
+            "quotedSingleSplit",
+            "entityQuotedDoubleSplit",
+            "entityQuotedSingleEnd",
+            "entityQuotedDoubleOpenerSplit",
+        ):
             for mode, result in self.cases["boundaries"][case_name].items():
                 with self.subTest(case=case_name, mode=mode):
                     self.assertIn('data-ref="/tmp/report.xlsx"', result["html"])
