66e92d7d0640ebe6e08860127ce032a8ef0833da
pxxD1998 <pxxD1998@gmail.com>
2026-08-23 18:13:11 +0800

    fix(renderer): handle parser-split escaped quote closer

--- static/messages.js
+++ static/messages.js
@@ -8,7 +8,7 @@
         ? normalized
         : '';
     };
-    const quotedRef=(candidate)=>String(candidate||'').replace(/&amp;(quot;|#39;)(?=[.,;:!?]*$)/,'&$1');
+    const quotedRef=(candidate)=>String(candidate||'').replace(/&amp;(quot|#39);?(?=[.,;:!?]*$)/,'&$1;');
     const localQuotedSource=quotedSource(before);
     if(localQuotedSource){
       return _mediaTokenParts(localQuotedSource,localQuotedSource.length,quotedRef(rawRef));
